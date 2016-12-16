fc6e810ba2cb575173a86014c4c6629392f0e8be
zsheill7 <zsheill7@gmail.com>
2016-12-16 08:43:10 -0800

    before changing app icon

--- SwiftRadio/NowPlayingViewController.swift
+++ SwiftRadio/NowPlayingViewController.swift
@@ -497,7 +497,7 @@ class NowPlayingViewController: UIViewController {
     }
     
     @IBAction func shareButtonPressed(_ sender: UIButton) {
-        let songToShare = "I'm listening to \(track.title) on \(currentStation.stationName) via Swift Radio Pro"
+        let songToShare = "I'm listening to \(track.title) on KMIH Radio via KMIH Radio App"
         let activityViewController = UIActivityViewController(activityItems: [songToShare, track.artworkImage!], applicationActivities: nil)
         present(activityViewController, animated: true, completion: nil)
     }
